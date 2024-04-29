81f070663ef50c2dc43886f95e47c163f536c84b
mari-carvalho <dide.carvalho@hotmail.com>
2024-04-29 16:54:45 -0300

    cn_tt

---
+++ TRABALHO_2_METNUM_II/main.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import sympy as sp 
+from CN_tt import CN
+from TRABALHO_2_METNUM_II import solver_gauss_seidel
+
+calc_CN = CN.calculate_CN_tt()
