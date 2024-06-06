8d53bfca62113b54ba16ca0407e36626b307a037
mari-carvalho <03950025081@edu.udesc.br>
2024-06-06 15:09:13 -0300

    trab 3

---
+++ TRABALHO_3_METNUM_II/sol_analitica_estacionario.py
@@ -0,0 +1,206 @@
+     
+class analitica():
+    
+    def calculate_analitica_nx10():
+        
+        import numpy as np
+        import math
+        import matplotlib.pyplot as plt
+        
+        # Propriedades do Material - Cobre
+        rho = 8.92 # g/cm^3
+        cp = 0.092 # cal/(g.ºC)
+        k = 0.95 # cal/(cm.s.ºC)
+        Lx = 10 # cm
+        Ly = 10 # cm
+        N = 200 # Números de termos da Série de Fourier
+        
+        nx = 10
+        ny = 10
+        
+        # Cálculos Iniciais
+        c = k/(rho*cp)
+        
+        # Define a função desejada:
+        def f(x):
+          return 20
+        
+        # Coeficiente da Série de Fourier
+        def fourier(f, Lx, Ly, N):
+          Bn = np.zeros(N)
+        
+          for n in range(1, N):
+        
+            def integrando(x):
+              return f(x)*np.sin(n*np.pi*x/Lx)
+        
+            Bn[n] = 2/(Lx*np.sinh(n*np.pi*Lx/Ly))*np.trapz(integrando(np.linspace(0,Lx,1000)),np.linspace(0,Lx,1000))
+        
+          return Bn
+        
+        # Solução da Equação do Calor:
+        def calor(x, y, Lx, Ly, N):
+          solucao = np.zeros_like(x) # np.zeros: cria uma matriz de zeros com o mesmo formato e tipo de dados de uma matriz de entrada fornecida.
+          coef = fourier(f, Lx, Ly, N)
+        
+          for n in range(1,N):
+        
+            solucao += coef[n]*np.sin(n*np.pi*x/Lx)*np.sinh(n*np.pi*y/Ly)
+        
+          return solucao
+        
+        # Define os valores de x e t
+        x = np.linspace(0, Lx, nx)
+        y = np.linspace(0, Ly, ny)
+        X, Y = np.meshgrid(x,y)
+        
+        Temperatura = calor(X, Y, Lx, Ly, N)
+        
+        # Gráfico 1
+        fig = plt.figure(figsize=(10, 10))
+        ax = fig.add_subplot(111, projection='3d')
+        surf = ax.plot_surface(X, Y, Temperatura, rstride=2, cstride=1, cmap=plt.cm.viridis, linewidth=0.2, alpha=1)
+        ax.set_xlabel('x (cm)')
+        ax.set_ylabel('y (cm)')
+        ax.set_zlabel('T(x,y) (ºC)')
+        plt.title('Equação do Calor 2D - Solução Analítica - nx=10')
+        fig.colorbar(surf, shrink=0.5, aspect=10)
+        #ax.view_init(30, 30)
+        
+        return Temperatura
+        
+    def calculate_analitica_nx20():
+        
+        import numpy as np
+        import math
+        import matplotlib.pyplot as plt
+        
+        # Propriedades do Material - Cobre
+        rho = 8.92 # g/cm^3
+        cp = 0.092 # cal/(g.ºC)
+        k = 0.95 # cal/(cm.s.ºC)
+        Lx = 10 # cm
+        Ly = 10 # cm
+        N = 200 # Números de termos da Série de Fourier
+        
+        nx = 20
+        ny = 10
+        
+        # Cálculos Iniciais
+        c = k/(rho*cp)
+        
+        # Define a função desejada:
+        def f(x):
+          return 20
+        
+        # Coeficiente da Série de Fourier
+        def fourier(f, Lx, Ly, N):
+          Bn = np.zeros(N)
+        
+          for n in range(1, N):
+        
+            def integrando(x):
+              return f(x)*np.sin(n*np.pi*x/Lx)
+        
+            Bn[n] = 2/(Lx*np.sinh(n*np.pi*Lx/Ly))*np.trapz(integrando(np.linspace(0,Lx,1000)),np.linspace(0,Lx,1000))
+        
+          return Bn
+        
+        # Solução da Equação do Calor:
+        def calor(x, y, Lx, Ly, N):
+          solucao = np.zeros_like(x) # np.zeros: cria uma matriz de zeros com o mesmo formato e tipo de dados de uma matriz de entrada fornecida.
+          coef = fourier(f, Lx, Ly, N)
+        
+          for n in range(1,N):
+        
+            solucao += coef[n]*np.sin(n*np.pi*x/Lx)*np.sinh(n*np.pi*y/Ly)
+        
+          return solucao
+        
+        # Define os valores de x e t
+        x = np.linspace(0, Lx, nx)
+        y = np.linspace(0, Ly, ny)
+        X, Y = np.meshgrid(x,y)
+        
+        Temperatura = calor(X, Y, Lx, Ly, N)
+        
+        # Gráfico 1
+        fig = plt.figure(figsize=(10, 10))
+        ax = fig.add_subplot(111, projection='3d')
+        surf = ax.plot_surface(X, Y, Temperatura, rstride=2, cstride=1, cmap=plt.cm.viridis, linewidth=0.2, alpha=1)
+        ax.set_xlabel('x (cm)')
+        ax.set_ylabel('y (cm)')
+        ax.set_zlabel('T(x,y) (ºC)')
+        plt.title('Equação do Calor 2D - Solução Analítica - nx=20')
+        fig.colorbar(surf, shrink=0.5, aspect=10)
+        #ax.view_init(30, 30)
+        return Temperatura
+        
+    def calculate_analitica_nx30():
+        
+        import numpy as np
+        import math
+        import matplotlib.pyplot as plt
+        
+        # Propriedades do Material - Cobre
+        rho = 8.92 # g/cm^3
+        cp = 0.092 # cal/(g.ºC)
+        k = 0.95 # cal/(cm.s.ºC)
+        Lx = 10 # cm
+        Ly = 10 # cm
+        N = 200 # Números de termos da Série de Fourier
+        
+        nx = 30
+        ny = 10
+        
+        # Cálculos Iniciais
+        c = k/(rho*cp)
+        
+        # Define a função desejada:
+        def f(x):
+          return 20
+        
+        # Coeficiente da Série de Fourier
+        def fourier(f, Lx, Ly, N):
+          Bn = np.zeros(N)
+        
+          for n in range(1, N):
+        
+            def integrando(x):
+              return f(x)*np.sin(n*np.pi*x/Lx)
+        
+            Bn[n] = 2/(Lx*np.sinh(n*np.pi*Lx/Ly))*np.trapz(integrando(np.linspace(0,Lx,1000)),np.linspace(0,Lx,1000))
+        
+          return Bn
+        
+        # Solução da Equação do Calor:
+        def calor(x, y, Lx, Ly, N):
+          solucao = np.zeros_like(x) # np.zeros: cria uma matriz de zeros com o mesmo formato e tipo de dados de uma matriz de entrada fornecida.
+          coef = fourier(f, Lx, Ly, N)
+        
+          for n in range(1,N):
+        
+            solucao += coef[n]*np.sin(n*np.pi*x/Lx)*np.sinh(n*np.pi*y/Ly)
+        
+          return solucao
+        
+        # Define os valores de x e t
+        x = np.linspace(0, Lx, nx)
+        y = np.linspace(0, Ly, ny)
+        X, Y = np.meshgrid(x,y)
+        
+        Temperatura = calor(X, Y, Lx, Ly, N)
+        
+        # Gráfico 1
+        fig = plt.figure(figsize=(10, 10))
+        ax = fig.add_subplot(111, projection='3d')
+        surf = ax.plot_surface(X, Y, Temperatura, rstride=2, cstride=1, cmap=plt.cm.viridis, linewidth=0.2, alpha=1)
+        ax.set_xlabel('x (cm)')
+        ax.set_ylabel('y (cm)')
+        ax.set_zlabel('T(x,y) (ºC)')
+        plt.title('Equação do Calor 2D - Solução Analítica - nx=30')
+        fig.colorbar(surf, shrink=0.5, aspect=10)
+        #ax.view_init(30, 30)
+        return Temperatura
+        
+        
---
+++ TRABALHO_3_METNUM_II/sol_numerica_estacionario.py
@@ -0,0 +1,687 @@
+class estacionario():
+    
+    def calculate_estacionario_nx10():
+
+        import numpy as np
+        import matplotlib.pyplot as plt
+        import time
+        
+        def Gauss_Seidel(A, b, x0, Eppara, maxit):
+            ne = len(b)
+            x = np.zeros(ne) if x0 is None else np.array(x0)
+            iter = 0
+            Epest = np.linspace(100,100,ne)
+        
+            while np.max(Epest) >= Eppara and iter <= maxit:
+                x_old = np.copy(x)
+        
+                for i in range(ne):
+                    sum1 = np.dot(A[i, :i], x[:i])
+                    sum2 = np.dot(A[i, i + 1:], x_old[i + 1:])
+                    x[i] = (b[i] - sum1 - sum2) / A[i, i]
+        
+                # Critério de parada
+                Epest = np.abs((x - x_old) / x) * 100
+        
+                iter += 1
+        
+            return x
+        
+        # Critério de Scarvorought, 1966
+        n = 12 # Números de algarismos significativos
+        Eppara = 0.5*10**(2-n) # Termo relativos
+        
+        # Número Máximo de Iterações
+        maxit = 1000
+        
+        # Propriedades do Material - Cobre
+        rho = 8.92  # g/cm^3
+        cp = 0.092  # cal/(g.ºC)
+        k = 0.95  # cal/(cm.s.ºC)
+        Lx = 10  # cm
+        Ly = 10  # cm
+        
+        # Dados Iniciais
+        Tn = 20  # ºC
+        Ts = 0  # ºC
+        Tw = 0  # ºC
+        Te = 0  # ºC
+        
+        # Parâmetros de simulação
+        nx = 10
+        ny = 10
+        N = nx*ny
+        
+        # Cálculos Iniciais
+        alpha = k/(rho*cp)
+        dx = Lx/nx
+        dy = Ly/ny
+        rxt = alpha/dx**2
+        ryt = alpha/dy**2
+        
+        rxt = dy**2
+        ryt = dx**2
+        
+        # Matriz Solução
+        TS = np.full((nx,ny),0)
+        A = np.zeros((N,N))
+        B = np.zeros(N)
+        
+        # Definindo os índices da matriz 2D para o vetor 1D
+        ind = np.arange(N).reshape(nx,ny)
+        
+        # Parte central
+        for i in range(1,ny-1):
+          for m in range(i*nx+1,(i+1)*nx-1):
+            Ap = 2*rxt + 2*ryt
+            Aw = -rxt
+            Ae = -rxt
+            As = -ryt
+            An = -ryt
+            S = 0
+        
+            A[m,m] = Ap
+            A[m,m-1] = Aw
+            A[m,m+1] = Ae
+            A[m,m+nx] = As
+            A[m,m-nx] = An
+            B[m] = S
+        
+        # Canto Superior Esquerdo
+        m = 0
+        Ap = 4*rxt + 4*ryt
+        Ae = -4/3*rxt
+        As = -4/3*ryt
+        S = 8/3*Tw*rxt + 8/3*Tn*ryt
+        
+        A[m,m] = Ap
+        A[m,m+1] = Ae
+        A[m,m+nx] = As
+        B[m] = S
+        
+        # Fronteira Norte
+        for m in range(1,nx-1):
+          Ap = 2*rxt + 4*ryt
+          Aw = -rxt
+          Ae = -rxt
+          As = -4/3*ryt
+          S = 8/3*Tn*ryt
+        
+          A[m,m] = Ap
+          A[m,m-1] = Aw
+          A[m,m+1] = Ae
+          A[m,m+nx] = As
+          B[m] = S
+        
+        # Canto Superior Direito
+        m = nx-1
+        Ap = 4*rxt + 4*ryt
+        Aw = -4/3*rxt
+        As = -4/3*ryt
+        S = 8/3*Te*rxt + 8/3*Tn*ryt
+        
+        A[m,m] = Ap
+        A[m,m-1] = Aw
+        A[m,m+nx] = As
+        B[m] = S
+        
+        # Fronteira Oeste
+        for m in range(nx,(ny-2)*nx+1,nx):
+          Ap = 2*rxt + 4*ryt
+          Ae = -4/3*rxt
+          An = -ryt
+          As = -ryt
+          S = 8/3*Tw*rxt
+        
+          A[m,m] = Ap
+          A[m,m+1] = Ae
+          A[m,m+nx] = As
+          A[m,m-nx] = An
+          B[m] = S
+        
+        # Fronteira Leste
+        for m in range(2*nx-1,(ny-1)*nx,nx):
+          Ap = 2*rxt + 4*ryt
+          Aw = -4/3*rxt
+          An = -ryt
+          As = -ryt
+          S = 8/3*Te*rxt
+        
+          A[m,m] = Ap
+          A[m,m-1] = Aw
+          A[m,m-nx] = An
+          A[m,m+nx] = As
+          B[m] = S
+        
+        # Canto Inferior Esquerdo
+        m = (ny-1)*nx
+        Ap = 4*rxt + 4*ryt
+        Ae = -4/3*rxt
+        As = -4/3*ryt
+        S = 8/3*Tw*rxt + 8/3*Ts*ryt
+        
+        A[m,m] = Ap
+        A[m,m+1] = Ae
+        A[m,m-nx] = An
+        B[m] = S
+        
+        # Fronteira Sul
+        for m in range((ny-1)*nx+1,ny*nx-1):
+          Ap = 2*rxt + 4*ryt
+          Aw = -rxt
+          Ae = -rxt
+          An = -4/3*ryt
+          S = 8/3*Ts*ryt
+        
+          A[m,m] = Ap
+          A[m,m-1] = Aw
+          A[m,m+1] = Ae
+          A[m,m-nx] = An
+          B[m] = S
+        
+        # Canto Inferior Direito
+        m = ny*nx-1
+        Ap = 4*rxt + 4*ryt
+        Ae = -4/3*rxt
+        As = -4/3*ryt
+        S = 8/3*Te*rxt + 8/3*Ts*ryt
+        
+        A[m,m] = Ap
+        A[m,m-1] = Aw
+        A[m,m-nx] = An
+        B[m] = S
+        
+        # Solução do sistema Linear
+        x0 = np.ones(N)
+        T = Gauss_Seidel(A, B, x0, Eppara, maxit)
+        #T = np.linalg.solve(A, B)
+        
+        TS = np.zeros((nx, ny), dtype=np.float64)
+        # Extração do valor da variável T
+        for i in range(nx):
+            for j in range(ny):
+                TS[i, j] = T[ind[i, j]]
+        
+        
+        # Plot
+        plt.figure()
+        plt.imshow(TS, extent=[0, Lx, 0, Ly], origin='lower', aspect='auto', cmap='hot')
+        plt.colorbar()
+        plt.xlabel('X (cm)')
+        plt.ylabel('Y (cm)')
+        plt.title('Distribuição de Temperatura - Solução Estacionário - nx=10')
+        
+        # Plotagem
+        X = np.linspace(0, Lx, nx)
+        Y = np.linspace(0, Ly, ny)
+        X, Y = np.meshgrid(X, Y)
+        
+        fig = plt.figure(figsize=(10, 10))
+        ax = fig.add_subplot(111, projection='3d')
+        surf = ax.plot_surface(Y, X, TS, rstride=2, cstride=1, cmap=plt.cm.viridis, linewidth=0.2, alpha=1)
+        ax.set_xlabel('x (cm)')
+        ax.set_ylabel('y (cm)')
+        ax.set_zlabel('T(x,y) (ºC)')
+        plt.title('Equação do Calor 2D - Solução Estacionário - nx=10')
+        fig.colorbar(surf, shrink=0.5, aspect=10)
+        ax.view_init(30, 30)
+        plt.show()
+        return TS
+    
+    def calculate_estacionario_nx20():
+    
+        import numpy as np
+        import matplotlib.pyplot as plt
+        import time
+        
+        def Gauss_Seidel(A, b, x0, Eppara, maxit):
+            ne = len(b)
+            x = np.zeros(ne) if x0 is None else np.array(x0)
+            iter = 0
+            Epest = np.linspace(100,100,ne)
+        
+            while np.max(Epest) >= Eppara and iter <= maxit:
+                x_old = np.copy(x)
+        
+                for i in range(ne):
+                    sum1 = np.dot(A[i, :i], x[:i])
+                    sum2 = np.dot(A[i, i + 1:], x_old[i + 1:])
+                    x[i] = (b[i] - sum1 - sum2) / A[i, i]
+        
+                # Critério de parada
+                Epest = np.abs((x - x_old) / x) * 100
+        
+                iter += 1
+        
+            return x
+        
+        # Critério de Scarvorought, 1966
+        n = 12 # Números de algarismos significativos
+        Eppara = 0.5*10**(2-n) # Termo relativos
+        
+        # Número Máximo de Iterações
+        maxit = 1000
+        
+        # Propriedades do Material - Cobre
+        rho = 8.92  # g/cm^3
+        cp = 0.092  # cal/(g.ºC)
+        k = 0.95  # cal/(cm.s.ºC)
+        Lx = 10  # cm
+        Ly = 10  # cm
+        
+        # Dados Iniciais
+        Tn = 20  # ºC
+        Ts = 0  # ºC
+        Tw = 0  # ºC
+        Te = 0  # ºC
+        
+        # Parâmetros de simulação
+        nx = 20
+        ny = 10
+        N = nx*ny
+        
+        # Cálculos Iniciais
+        alpha = k/(rho*cp)
+        dx = Lx/nx
+        dy = Ly/ny
+        rxt = alpha/dx**2
+        ryt = alpha/dy**2
+        
+        rxt = dy**2
+        ryt = dx**2
+        
+        # Matriz Solução
+        TS = np.full((nx,ny),0)
+        A = np.zeros((N,N))
+        B = np.zeros(N)
+        
+        # Definindo os índices da matriz 2D para o vetor 1D
+        ind = np.arange(N).reshape(nx,ny)
+        
+        # Parte central
+        for i in range(1,ny-1):
+          for m in range(i*nx+1,(i+1)*nx-1):
+            Ap = 2*rxt + 2*ryt
+            Aw = -rxt
+            Ae = -rxt
+            As = -ryt
+            An = -ryt
+            S = 0
+        
+            A[m,m] = Ap
+            A[m,m-1] = Aw
+            A[m,m+1] = Ae
+            A[m,m+nx] = As
+            A[m,m-nx] = An
+            B[m] = S
+        
+        # Canto Superior Esquerdo
+        m = 0
+        Ap = 4*rxt + 4*ryt
+        Ae = -4/3*rxt
+        As = -4/3*ryt
+        S = 8/3*Tw*rxt + 8/3*Tn*ryt
+        
+        A[m,m] = Ap
+        A[m,m+1] = Ae
+        A[m,m+nx] = As
+        B[m] = S
+        
+        # Fronteira Norte
+        for m in range(1,nx-1):
+          Ap = 2*rxt + 4*ryt
+          Aw = -rxt
+          Ae = -rxt
+          As = -4/3*ryt
+          S = 8/3*Tn*ryt
+        
+          A[m,m] = Ap
+          A[m,m-1] = Aw
+          A[m,m+1] = Ae
+          A[m,m+nx] = As
+          B[m] = S
+        
+        # Canto Superior Direito
+        m = nx-1
+        Ap = 4*rxt + 4*ryt
+        Aw = -4/3*rxt
+        As = -4/3*ryt
+        S = 8/3*Te*rxt + 8/3*Tn*ryt
+        
+        A[m,m] = Ap
+        A[m,m-1] = Aw
+        A[m,m+nx] = As
+        B[m] = S
+        
+        # Fronteira Oeste
+        for m in range(nx,(ny-2)*nx+1,nx):
+          Ap = 2*rxt + 4*ryt
+          Ae = -4/3*rxt
+          An = -ryt
+          As = -ryt
+          S = 8/3*Tw*rxt
+        
+          A[m,m] = Ap
+          A[m,m+1] = Ae
+          A[m,m+nx] = As
+          A[m,m-nx] = An
+          B[m] = S
+        
+        # Fronteira Leste
+        for m in range(2*nx-1,(ny-1)*nx,nx):
+          Ap = 2*rxt + 4*ryt
+          Aw = -4/3*rxt
+          An = -ryt
+          As = -ryt
+          S = 8/3*Te*rxt
+        
+          A[m,m] = Ap
+          A[m,m-1] = Aw
+          A[m,m-nx] = An
+          A[m,m+nx] = As
+          B[m] = S
+        
+        # Canto Inferior Esquerdo
+        m = (ny-1)*nx
+        Ap = 4*rxt + 4*ryt
+        Ae = -4/3*rxt
+        As = -4/3*ryt
+        S = 8/3*Tw*rxt + 8/3*Ts*ryt
+        
+        A[m,m] = Ap
+        A[m,m+1] = Ae
+        A[m,m-nx] = An
+        B[m] = S
+        
+        # Fronteira Sul
+        for m in range((ny-1)*nx+1,ny*nx-1):
+          Ap = 2*rxt + 4*ryt
+          Aw = -rxt
+          Ae = -rxt
+          An = -4/3*ryt
+          S = 8/3*Ts*ryt
+        
+          A[m,m] = Ap
+          A[m,m-1] = Aw
+          A[m,m+1] = Ae
+          A[m,m-nx] = An
+          B[m] = S
+        
+        # Canto Inferior Direito
+        m = ny*nx-1
+        Ap = 4*rxt + 4*ryt
+        Ae = -4/3*rxt
+        As = -4/3*ryt
+        S = 8/3*Te*rxt + 8/3*Ts*ryt
+        
+        A[m,m] = Ap
+        A[m,m-1] = Aw
+        A[m,m-nx] = An
+        B[m] = S
+        
+        # Solução do sistema Linear
+        x0 = np.ones(N)
+        T = Gauss_Seidel(A, B, x0, Eppara, maxit)
+        #T = np.linalg.solve(A, B)
+        
+        TS = np.zeros((nx, ny), dtype=np.float64)
+        # Extração do valor da variável T
+        for i in range(nx):
+            for j in range(ny):
+                TS[i, j] = T[ind[i, j]]
+        
+        
+        # Plot
+        plt.figure()
+        plt.imshow(TS, extent=[0, Lx, 0, Ly], origin='lower', aspect='auto', cmap='hot')
+        plt.colorbar()
+        plt.xlabel('X (cm)')
+        plt.ylabel('Y (cm)')
+        plt.title('Distribuição de Temperatura - Solução Estacionário - nx=20')
+        
+        # Plotagem
+        X = np.linspace(0, Lx, nx)
+        Y = np.linspace(0, Ly, ny)
+        X, Y = np.meshgrid(X, Y)
+        
+        fig = plt.figure(figsize=(10, 10))
+        ax = fig.add_subplot(111, projection='3d')
+        surf = ax.plot_surface(Y, X, TS, rstride=2, cstride=1, cmap=plt.cm.viridis, linewidth=0.2, alpha=1)
+        ax.set_xlabel('x (cm)')
+        ax.set_ylabel('y (cm)')
+        ax.set_zlabel('T(x,y) (ºC)')
+        plt.title('Equação do Calor 2D - Solução Estacionário - nx=20')
+        fig.colorbar(surf, shrink=0.5, aspect=10)
+        ax.view_init(30, 30)
+        plt.show()
+        return TS
+        
+    def calculate_estacionario_nx30():
+    
+        import numpy as np
+        import matplotlib.pyplot as plt
+        import time
+        
+        def Gauss_Seidel(A, b, x0, Eppara, maxit):
+            ne = len(b)
+            x = np.zeros(ne) if x0 is None else np.array(x0)
+            iter = 0
+            Epest = np.linspace(100,100,ne)
+        
+            while np.max(Epest) >= Eppara and iter <= maxit:
+                x_old = np.copy(x)
+        
+                for i in range(ne):
+                    sum1 = np.dot(A[i, :i], x[:i])
+                    sum2 = np.dot(A[i, i + 1:], x_old[i + 1:])
+                    x[i] = (b[i] - sum1 - sum2) / A[i, i]
+        
+                # Critério de parada
+                Epest = np.abs((x - x_old) / x) * 100
+        
+                iter += 1
+        
+            return x
+        
+        # Critério de Scarvorought, 1966
+        n = 12 # Números de algarismos significativos
+        Eppara = 0.5*10**(2-n) # Termo relativos
+        
+        # Número Máximo de Iterações
+        maxit = 1000
+        
+        # Propriedades do Material - Cobre
+        rho = 8.92  # g/cm^3
+        cp = 0.092  # cal/(g.ºC)
+        k = 0.95  # cal/(cm.s.ºC)
+        Lx = 10  # cm
+        Ly = 10  # cm
+        
+        # Dados Iniciais
+        Tn = 20  # ºC
+        Ts = 0  # ºC
+        Tw = 0  # ºC
+        Te = 0  # ºC
+        
+        # Parâmetros de simulação
+        nx = 30
+        ny = 10
+        N = nx*ny
+        
+        # Cálculos Iniciais
+        alpha = k/(rho*cp)
+        dx = Lx/nx
+        dy = Ly/ny
+        rxt = alpha/dx**2
+        ryt = alpha/dy**2
+        
+        rxt = dy**2
+        ryt = dx**2
+        
+        # Matriz Solução
+        TS = np.full((nx,ny),0)
+        A = np.zeros((N,N))
+        B = np.zeros(N)
+        
+        # Definindo os índices da matriz 2D para o vetor 1D
+        ind = np.arange(N).reshape(nx,ny)
+        
+        # Parte central
+        for i in range(1,ny-1):
+          for m in range(i*nx+1,(i+1)*nx-1):
+            Ap = 2*rxt + 2*ryt
+            Aw = -rxt
+            Ae = -rxt
+            As = -ryt
+            An = -ryt
+            S = 0
+        
+            A[m,m] = Ap
+            A[m,m-1] = Aw
+            A[m,m+1] = Ae
+            A[m,m+nx] = As
+            A[m,m-nx] = An
+            B[m] = S
+        
+        # Canto Superior Esquerdo
+        m = 0
+        Ap = 4*rxt + 4*ryt
+        Ae = -4/3*rxt
+        As = -4/3*ryt
+        S = 8/3*Tw*rxt + 8/3*Tn*ryt
+        
+        A[m,m] = Ap
+        A[m,m+1] = Ae
+        A[m,m+nx] = As
+        B[m] = S
+        
+        # Fronteira Norte
+        for m in range(1,nx-1):
+          Ap = 2*rxt + 4*ryt
+          Aw = -rxt
+          Ae = -rxt
+          As = -4/3*ryt
+          S = 8/3*Tn*ryt
+        
+          A[m,m] = Ap
+          A[m,m-1] = Aw
+          A[m,m+1] = Ae
+          A[m,m+nx] = As
+          B[m] = S
+        
+        # Canto Superior Direito
+        m = nx-1
+        Ap = 4*rxt + 4*ryt
+        Aw = -4/3*rxt
+        As = -4/3*ryt
+        S = 8/3*Te*rxt + 8/3*Tn*ryt
+        
+        A[m,m] = Ap
+        A[m,m-1] = Aw
+        A[m,m+nx] = As
+        B[m] = S
+        
+        # Fronteira Oeste
+        for m in range(nx,(ny-2)*nx+1,nx):
+          Ap = 2*rxt + 4*ryt
+          Ae = -4/3*rxt
+          An = -ryt
+          As = -ryt
+          S = 8/3*Tw*rxt
+        
+          A[m,m] = Ap
+          A[m,m+1] = Ae
+          A[m,m+nx] = As
+          A[m,m-nx] = An
+          B[m] = S
+        
+        # Fronteira Leste
+        for m in range(2*nx-1,(ny-1)*nx,nx):
+          Ap = 2*rxt + 4*ryt
+          Aw = -4/3*rxt
+          An = -ryt
+          As = -ryt
+          S = 8/3*Te*rxt
+        
+          A[m,m] = Ap
+          A[m,m-1] = Aw
+          A[m,m-nx] = An
+          A[m,m+nx] = As
+          B[m] = S
+        
+        # Canto Inferior Esquerdo
+        m = (ny-1)*nx
+        Ap = 4*rxt + 4*ryt
+        Ae = -4/3*rxt
+        As = -4/3*ryt
+        S = 8/3*Tw*rxt + 8/3*Ts*ryt
+        
+        A[m,m] = Ap
+        A[m,m+1] = Ae
+        A[m,m-nx] = An
+        B[m] = S
+        
+        # Fronteira Sul
+        for m in range((ny-1)*nx+1,ny*nx-1):
+          Ap = 2*rxt + 4*ryt
+          Aw = -rxt
+          Ae = -rxt
+          An = -4/3*ryt
+          S = 8/3*Ts*ryt
+        
+          A[m,m] = Ap
+          A[m,m-1] = Aw
+          A[m,m+1] = Ae
+          A[m,m-nx] = An
+          B[m] = S
+        
+        # Canto Inferior Direito
+        m = ny*nx-1
+        Ap = 4*rxt + 4*ryt
+        Ae = -4/3*rxt
+        As = -4/3*ryt
+        S = 8/3*Te*rxt + 8/3*Ts*ryt
+        
+        A[m,m] = Ap
+        A[m,m-1] = Aw
+        A[m,m-nx] = An
+        B[m] = S
+        
+        # Solução do sistema Linear
+        x0 = np.ones(N)
+        T = Gauss_Seidel(A, B, x0, Eppara, maxit)
+        #T = np.linalg.solve(A, B)
+        
+        TS = np.zeros((nx, ny), dtype=np.float64)
+        # Extração do valor da variável T
+        for i in range(nx):
+            for j in range(ny):
+                TS[i, j] = T[ind[i, j]]
+        
+        
+        # Plot
+        plt.figure()
+        plt.imshow(TS, extent=[0, Lx, 0, Ly], origin='lower', aspect='auto', cmap='hot')
+        plt.colorbar()
+        plt.xlabel('X (cm)')
+        plt.ylabel('Y (cm)')
+        plt.title('Distribuição de Temperatura - Solução Estacionário - nx=30')
+        
+        # Plotagem
+        X = np.linspace(0, Lx, nx)
+        Y = np.linspace(0, Ly, ny)
+        X, Y = np.meshgrid(X, Y)
+        
+        fig = plt.figure(figsize=(10, 10))
+        ax = fig.add_subplot(111, projection='3d')
+        surf = ax.plot_surface(Y, X, TS, rstride=2, cstride=1, cmap=plt.cm.viridis, linewidth=0.2, alpha=1)
+        ax.set_xlabel('x (cm)')
+        ax.set_ylabel('y (cm)')
+        ax.set_zlabel('T(x,y) (ºC)')
+        plt.title('Equação do Calor 2D - Solução Estacionário - nx=30')
+        fig.colorbar(surf, shrink=0.5, aspect=10)
+        ax.view_init(30, 30)
+        plt.show()
+        return TS
+        
+        
